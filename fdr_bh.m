0485c8df82bf60892a4c5a003789a1ccae28ad89
MJP <mprerau@bwh.harvard.edu>
2023-06-21 14:33:42 -0400

    light edits to fdr_bh and forced it to remove invalid pvalues

--- fdr_bh.m
+++ fdr_bh.m
@@ -122,6 +122,8 @@
 % University of California, San Diego
 % March 24, 2010
 
+%Edited
+
 %%%%%%%%%%%%%%%% REVISION LOG %%%%%%%%%%%%%%%%%
 %
 % 5/7/2010-Added FDR adjusted p-values
@@ -130,39 +132,46 @@
 
 function [h, crit_p, adj_ci_cvrg, adj_p]=fdr_bh(pvals,q,method,report)
 
-if nargin<1,
+if nargin<1
     error('You need to provide a vector or matrix of p-values.');
 else
-    if ~isempty(find(pvals<0,1)),
+    if any(pvals<0)
         error('Some p-values are less than 0.');
-    elseif ~isempty(find(pvals>1,1)),
+    elseif any(pvals>1)
         error('Some p-values are greater than 1.');
     end
 end
 
-if nargin<2,
+%Check for nan/inf and remove them
+inds = isfinite(pvals);
+if any(~inds)
+    warning('Contains Nan/Inf values. Removing from invalid pvals from the list.')
+    pvals = pvals(inds);
+end
+
+if nargin<2
     q=.05;
 end
 
-if nargin<3,
+if nargin<3
     method='pdep';
 end
 
-if nargin<4,
+if nargin<4
     report='no';
 end
 
 s=size(pvals);
-if (length(s)>2) || s(1)>1,
+if (length(s)>2) || s(1)>1
     [p_sorted, sort_ids]=sort(reshape(pvals,1,prod(s)));
 else
     %p-values are already a row vector
     [p_sorted, sort_ids]=sort(pvals);
 end
-[dummy, unsort_ids]=sort(sort_ids); %indexes to return p_sorted to pvals order
+[~, unsort_ids]=sort(sort_ids); %indexes to return p_sorted to pvals order
 m=length(p_sorted); %number of tests
 
-if strcmpi(method,'pdep'),
+if strcmpi(method,'pdep')
     %BH procedure for independence or positive dependence
     thresh=(1:m)*q/m;
     wtd_p=m*p_sorted./(1:m);
@@ -171,14 +180,14 @@ elseif strcmpi(method,'dep')
     %BH procedure for any dependency structure
     denom=m*sum(1./(1:m));
     thresh=(1:m)*q/denom;
-    wtd_p=denom*p_sorted./[1:m];
+    wtd_p=denom*p_sorted./(1:m);
     %Note, it can produce adjusted p-values greater than 1!
     %compute adjusted p-values
 else
     error('Argument ''method'' needs to be ''pdep'' or ''dep''.');
 end
 
-if nargout>3,
+if nargout>3
     %compute adjusted p-values; This can be a bit computationally intensive
     adj_p=zeros(1,m)*NaN;
     [wtd_p_sorted, wtd_p_sindex] = sort( wtd_p );
@@ -189,15 +198,15 @@ if nargout>3,
             nextfill = wtd_p_sindex(k)+1;
             if nextfill>m
                 break;
-            end;
-        end;
-    end;
+            end
+        end
+    end
     adj_p=reshape(adj_p(unsort_ids),s);
 end
 
 rej=p_sorted<=thresh;
 max_id=find(rej,1,'last'); %find greatest significant pvalue
-if isempty(max_id),
+if isempty(max_id)
     crit_p=0;
     h=pvals*0;
     adj_ci_cvrg=NaN;
@@ -207,14 +216,14 @@ else
     adj_ci_cvrg=1-thresh(max_id);
 end
 
-if strcmpi(report,'yes'),
+if strcmpi(report,'yes')
     n_sig=sum(p_sorted<=crit_p);
-    if n_sig==1,
+    if n_sig==1
         fprintf('Out of %d tests, %d is significant using a false discovery rate of %f.\n',m,n_sig,q);
     else
         fprintf('Out of %d tests, %d are significant using a false discovery rate of %f.\n',m,n_sig,q);
     end
-    if strcmpi(method,'pdep'),
+    if strcmpi(method,'pdep')
         fprintf('FDR/FCR procedure used is guaranteed valid for independent or positively dependent tests.\n');
     else
         fprintf('FDR/FCR procedure used is guaranteed valid for independent or dependent tests.\n');
